c27d3a336264f40b6c6b20b93b0ce137e9a24e78
ATILAY YILMAZ <atiyil@gmail.com>
2018-04-10 16:32:28 -0400

    added Serialization support

--- singleton.java
+++ singleton.java
@@ -1,10 +1,14 @@
-class Singleton{
+class Singleton {
   public String str;
   private static Singleton instance = null;
-  private Singleton(){str = "Hello, I am a singleton!";}
-  public static synchronized Singleton getSingleInstance(){
+  private Singleton() {str = "Hello, I am a singleton!";}
+  public static synchronized Singleton getSingleInstance() {
     if (instance == null)
       instance = new Singleton(); 
     return instance;
   }
+  /* Uncomment below to add Serialization support
+  private Object readResolve() {
+   return instance;
+  }*/
 }
